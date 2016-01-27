3e51458e619adbe4dfb86100546f8dbf3a6b5679
David J. Arias López M <querquobad69@gmail.com>
2016-01-27 11:44:33 -0600

    Modificar clase input para bootstrap
    Para BootStrap es necesario que los inputs estén dentro de un div con su
    label se corrige loop infinito al renderear los inputs.

--- input.php
+++ input.php
@@ -3,46 +3,57 @@
 class input extends element {
 	private $label;
 	private $div;
+	private $input;
+
 	public function __construct($att) {
 		if (!is_a($this,'input') && isset($att['tag']) && $att['tag'] !== 'input')
 			throw new RuntimeException('No se puede crear un elemento '.$att['tag'].' con la clase input');
 		// Solo sobreescribimos el tag en caso que no exista puede ser una clase hija como select que ya pone un TAG
 		if (!isset($att['tag'])) $att['tag'] = 'input'; 
-		$this->addAtributo('class','form-control');
-		parent::__construct($att);
+		/*
+		 * Creamos el input "real" y le quitamos el atributo label en caso que lo tenga
+		 */
+		$this->input = new element($att);
+		$this->input->addAtributo('class','form-control');
+		if($this->input->getAtributo('label')) $this->input->delAtributo('label');
 		if (isset($att['label']) && is_string($att['label'])) {
 			$this->addLabel($att['label']);
 		} else if (isset($att['label']) && is_a($att['label'],'element')) {
 			$this->label = $att['label'];
 		}
 		unset($att['label']);
-		$this->delAtributo('label');
 		$this->div = new element(array(
 			'tag' => 'div',
 			'class' => 'form-group',
 			'id' => false, //quiza debamos de ponerle algo?
 		));
 	}
+
 	protected function getDiv() {
 		return $this->div;
 	}
+
 	protected function addLabel($label) {
-		if ($this->getAtributo('id') == null || $this->getAtributo('id') === false) throw new RuntimeException('No se puede agregar una etiqueta a un elemento sin ID');
-		if ($this->label != null) error_log('Sobreescribiendo la etiqueta del elemento '.$this->getAtributo('id'));
+		if ($this->input->getAtributo('id') == null || $this->input->getAtributo('id') === false)
+			throw new RuntimeException('No se puede agregar una etiqueta a un elemento sin ID');
+		if ($this->label != null) error_log('Sobreescribiendo la etiqueta del elemento '.$this->input->getAtributo('id'));
 		$this->label = new element(array(
 			'tag' => 'label',
 			'_text' => $label,
-			'for' => $this->getAtributo('id')
+			'for' => $this->input->getAtributo('id')
 		));
 	}
+
 	protected function getLabel() {
 		if(is_a($this->label,'element')) return $this->label;
 	}
+
 	public function render() {
 		$this->div->addElement($this->label);
-		$this->div->addElement($this);
+		$this->div->addElement($this->input);
 		return $this->div->render();
 	}
+
 }
 
 ?>
